aacc8ff4a429c50c897ca7ece8e6f71317854103
LiamWoods <woodsie123@hotmail.co.uk>
2020-02-18 20:55:30 +0000

    Added usage of inheritance method, changed config for clarity

---
+++ DependencyInjectionDemo/InheritanceOverFactory/NewController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DependencyInjectionDemo.InheritanceOverFactory
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class NewController : ControllerBase
+    {
+        private readonly IRepo<NewConfig> _repo;
+
+        public NewController(IRepo<NewConfig> repo)
+        {
+            _repo = repo;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var result = _repo.GetSomething();
+            return Ok(result);
+        }
+    }
+}
